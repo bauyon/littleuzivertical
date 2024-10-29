45bf997b3112ea0d399895473540575396cddc12
bauyon <151402740+bauyon@users.noreply.github.com>
2024-10-29 16:08:07 -0600

    Add files via upload

--- chicken.c
+++ chicken.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
-#include <string.h>
-#include <unistd.h>
-#include <math.h>
-
-
-int main(){
-    int i = 10;
-do {
-  printf("%d\n", i);
-} while (i < 10);
+int *foo(int *p, int *q)
+{
+  *p = *q + 2;
+  return p;
+}
+int main(void)
+{
+  int i, j, *p, *q;
+  i = 4;
+  j = 6;
+  q = foo(&i, &j);
+  p = q;
+  *p = i + 1;
+  printf("%d, %d, %d, %d", i, j, *p, *q);
 }
